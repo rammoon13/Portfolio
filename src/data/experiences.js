4408b61076b7505fc43736d00dd0ee47207cd1f9
rammoon13 <herreraroblesramon@gmail.com>
2025-07-11 15:24:10 +0200

    experiencia actualizada

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -4,15 +4,15 @@ const experiences = [
     id: 1,
     role: 'Desarrollador',
     company: 'Controlnet',
-    period: 'Feb 2024 - Jun 2025',
+    period: 'Mar 2024 - Jun 2025',
     description: 'Desarrollo de software a medida con .NET, MySQL y MudBlazor.',
   },
   {
     id: 2,
     role: 'Profesor aula matinal',
-    company: 'Centro educativo',
-    period: '1 mes',
-    description: 'Responsable de actividades y cuidado de ni\u00f1os en horario matinal.',
+    company: 'Os Zagales',
+    period: 'May 2023 - Jun 2023',
+    description: 'Responsable de actividades y cuidado de niños en horario matinal.',
   },
 ]
 
